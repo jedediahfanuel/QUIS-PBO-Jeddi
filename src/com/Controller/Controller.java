9067339a7b708d7ee4b9b5d9a4cdc3c8f069bb24
Jeddi212 <jedediahf51@gmail.com>
2020-11-17 19:16:59 +0700

    Prepare Commit

---
+++ src/com/Controller/Controller.java
@@ -0,0 +1,32 @@
+package com.Controller;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class Controller {
+
+    static DatabaseHandler conn = new DatabaseHandler();
+
+    // SELECT ALL from table dataKTP
+//    public static ArrayList<KTP> getAllKTP() {
+//        ArrayList<KTP> listKTP = new ArrayList<>();
+//        conn.connect();
+//        String query = "SELECT * FROM dataKTP";
+//        try {
+//            Statement stmt = conn.con.createStatement();
+//            ResultSet rs = stmt.executeQuery(query);
+//            while (rs.next()) {
+//                KTP ktp = new KTP();
+//                ktp.setNIK(rs.getString("NIK"));
+//                ktp.setNama(rs.getString("Nama"));
+//                listKTP.add(ktp);
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return (listKTP);
+//    }
+
+}
